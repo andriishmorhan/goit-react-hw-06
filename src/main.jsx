83fa903d49a6938702e04eb08269afc724b7a37b
Andrew Shmorhan <140952286+andriishmorhan@users.noreply.github.com>
2024-04-01 22:22:56 +0200

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { PersisGate } from "redux-persist/integration/react";
-import { store } from "./redux/store";
-import App from "./components/App/App";
+import { App } from "./components/App/App";
 import "./index.css";
+import { persistor, store } from "./redux/store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
